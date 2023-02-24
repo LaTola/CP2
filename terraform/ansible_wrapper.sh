b7de89b785946a03df010a692b013e13356199be
Wilbert Iglesias <wiglesias@gmail.com>
2023-02-23 23:56:41 -0300

    added some k8s terraform outputs

--- terraform/ansible_wrapper.sh
+++ terraform/ansible_wrapper.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # wrapper script para invocar ansible y sobreescribir los parametros 
-ansible_host=$(terraform output -raw podman_public_ip_address)
+vm_host=$(terraform output -raw podman_public_ip_address)
 acrcp2_url=$(terraform output -raw acr_login_server)
 acrcp2_pwd=$(terraform output -raw acr_admin_password)
 acrcp2_user=$(terraform output -raw acr_admin_username)
+acrcp2_kube_config=$(terraform output -raw cp2aks_kube_config)
+acrcp2_k8s_fqdn=$(terraform output -raw cp2aks_host)
 
 cd ../ansible
-ansible-playbook setup.yml \
+ansible-playbook $1 setup.yml \
     --extra-vars \
-    "podman ansible_host=$ansible_host \
+    "podman ansible_host=$vm_host \
     acrcp2_url=$acrcp2_url \
     acrcp2_pwd=$acrcp2_pwd \
-    acrcp2_user=$acrcp2_user"
+    acrcp2_user=$acrcp2_user \
+    acrcp2_kube_config=$acrcp2_kube_config" \
+    #ak8s ansible_host=$acrcp2_k8s_fqdn"
 cd -
