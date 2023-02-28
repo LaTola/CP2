a8463b10b154c72d4dfd5003b398499b2d998fcc
Wilbert Iglesias <wiglesias@gmail.com>
2023-02-27 22:22:20 -0300

    Se agrega dump temporal de la configuracion del cluster de aks

--- terraform/ansible_wrapper.sh
+++ terraform/ansible_wrapper.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+# Dump temporal de la configuracion de AKS
+k8s_temp_conf=$(mktemp)
+chmod 0600 $k8s_temp_conf
+$(terraform output -raw cp2aks_kube_config >$k8s_temp_conf)
+
 # wrapper script para invocar ansible y sobreescribir los parametros 
 vm_host=$(terraform output -raw podman_public_ip_address)
 acrcp2_url=$(terraform output -raw acr_login_server)
 acrcp2_pwd=$(terraform output -raw acr_admin_password)
 acrcp2_user=$(terraform output -raw acr_admin_username)
-acrcp2_kube_config=$(terraform output -raw cp2aks_kube_config)
-acrcp2_k8s_fqdn=$(terraform output -raw cp2aks_host)
 
+# Playbook dir
 cd ../ansible
 ansible-playbook $1 setup.yml \
     --extra-vars \
@@ -15,6 +19,9 @@ ansible-playbook $1 setup.yml \
     acrcp2_url=$acrcp2_url \
     acrcp2_pwd=$acrcp2_pwd \
     acrcp2_user=$acrcp2_user \
-    acrcp2_kube_config=$acrcp2_kube_config" \
-    #ak8s ansible_host=$acrcp2_k8s_fqdn"
+    ak8s_kube_config=$k8s_temp_conf"
+
+# Se vuelve al directorio de terraform 
 cd -
+# el modulo 06-k8s.yml se encarga de eliminar las copias temporales 
+# de la configuracion del cluster de kubernetes (local y remota)
